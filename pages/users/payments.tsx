aabdabb7c932b874c9341d988e5a7bf72b88b23a
Sai Dinesh <saidinesh@MAC43.local>
2023-05-26 20:15:55 +0530

    added handleinput

--- pages/users/payments.tsx
+++ pages/users/payments.tsx
@@ -1,10 +1,52 @@
-import { Box, Input } from "@chakra-ui/react";
+import {
+  Box,
+  Button,
+  HStack,
+  Input,
+  Tab,
+  TabList,
+  TabPanel,
+  TabPanels,
+  Tabs,
+  Text,
+  VStack,
+} from "@chakra-ui/react";
 import React, { useState } from "react";
 
+type paymentInfo = {
+  address: string;
+  zip: number;
+  phone: number;
+
+  sixteendigitnumber: number;
+  date: number;
+  name: string;
+  cvv: number;
+
+  upi: string;
+  cod: boolean;
+};
+
 const Payments = () => {
-  const [address, setAddress] = useState<string>("");
-  const handleAddress = (e: any) => {
-    setAddress(e.target.value);
+  const [paymentInfo, setPaymentInfo] = useState<paymentInfo>({
+    address: "",
+    zip: 0,
+    phone: 0,
+    sixteendigitnumber: 0,
+    date: 0,
+    name: "",
+    cvv: 0,
+    upi: "",
+    cod: false,
+  });
+  const handleData = (e: any) => {
+    const { name, value } = e.target;
+    const updatedPayment: any = { ...paymentInfo };
+
+    updatedPayment[name] = value;
+
+    setPaymentInfo(updatedPayment);
+    console.log(updatedPayment, "firstconsole");
   };
   return (
     <Box
@@ -26,10 +68,11 @@ const Payments = () => {
         <Box>
           <Input
             type="text"
-            value={address}
+            name="address"
+            value={paymentInfo.address}
             w={{ base: "100%", sm: "100%", md: "60%", lg: "40%" }}
-            onChange={handleAddress}
             borderColor={"1px solid gray"}
+            onChange={handleData}
           ></Input>
         </Box>
       </Box>
@@ -37,8 +80,136 @@ const Payments = () => {
         w={{ base: "100%", sm: "100%", md: "60%", lg: "60%" }}
         h="auto"
         bg="white"
+        display={"flex"}
+        flexDir={"row"}
+        p="5"
       >
-        HI
+        <Box
+          w={{ base: "100%", sm: "100%", md: "60%", lg: "60%" }}
+          display={"flex"}
+          flexDir={"column"}
+        >
+          <HStack>
+            <VStack alignItems={"flex-start"}>
+              <Box>ZIP Code</Box>
+              <Box>
+                <Input
+                  type="text"
+                  name="zip"
+                  value={paymentInfo.zip}
+                  w="100%"
+                  borderColor={"1px solid gray"}
+                  onChange={handleData}
+                ></Input>
+              </Box>
+            </VStack>
+            <VStack alignItems={"flex-start"}>
+              <Text>Phone Number</Text>
+              <Box>
+                <Input
+                  name="phone"
+                  value={paymentInfo.phone}
+                  type="text"
+                  w="100%"
+                  borderColor={"1px solid gray"}
+                  onChange={handleData}
+                ></Input>
+              </Box>
+            </VStack>
+          </HStack>
+        </Box>
+      </Box>
+
+      <Box
+        w={{ base: "100%", sm: "100%", md: "60%", lg: "60%" }}
+        h="350px"
+        bg="blue.100"
+        display={"flex"}
+        flexDir={"row"}
+        p="5"
+      >
+        <Box
+          w={{ base: "100%", sm: "100%", md: "60%", lg: "60%" }}
+          display={"flex"}
+          flexDir={"column"}
+          border={"1px solid red"}
+        >
+          <Tabs variant="enclosed">
+            <TabList>
+              <Tab>Credit Card</Tab>
+              <Tab>UPI</Tab>
+              <Tab>COD</Tab>
+            </TabList>
+
+            <TabPanels>
+              <TabPanel>
+                <Box>CREDIT CARD</Box>
+                <HStack>
+                  <Input
+                    type="text"
+                    name="sixteendigitnumber"
+                    value={paymentInfo.sixteendigitnumber}
+                    w={{ base: "100%", sm: "100%", md: "60%", lg: "40%" }}
+                    placeholder="16 digit number"
+                    borderColor={"1px solid gray"}
+                    onChange={handleData}
+                  ></Input>
+
+                  <Box w="15px"></Box>
+
+                  <Input
+                    type="text"
+                    w={{ base: "50%", sm: "50%", md: "30%", lg: "20%" }}
+                    placeholder="MM/YY"
+                    borderColor={"1px solid gray"}
+                    value={paymentInfo.date}
+                    name="date"
+                    onChange={handleData}
+                  ></Input>
+                </HStack>
+                <HStack mt="5%">
+                  <Input
+                    type="text"
+                    placeholder="Card Holder Name"
+                    w={{ base: "100%", sm: "100%", md: "60%", lg: "40%" }}
+                    borderColor={"1px solid gray"}
+                    value={paymentInfo.name}
+                    name="name"
+                    onChange={handleData}
+                  ></Input>
+
+                  <Box w="15px"></Box>
+
+                  <Input
+                    type="text"
+                    w={{ base: "50%", sm: "50%", md: "30%", lg: "20%" }}
+                    placeholder="CVV"
+                    value={paymentInfo.cvv}
+                    name="cvv"
+                    borderColor={"1px solid gray"}
+                    onChange={handleData}
+                  ></Input>
+                </HStack>
+              </TabPanel>
+              <TabPanel>
+                <HStack>
+                  <Input
+                    type="text"
+                    w={{ base: "100%", sm: "100%", md: "60%", lg: "40%" }}
+                    placeholder="UPI ID"
+                    borderColor={"1px solid gray"}
+                    value={paymentInfo.upi}
+                    name="upi"
+                    onChange={handleData}
+                  ></Input>
+                </HStack>
+              </TabPanel>
+              <TabPanel>
+                <Button>Confirm COD</Button>
+              </TabPanel>
+            </TabPanels>
+          </Tabs>
+        </Box>
       </Box>
     </Box>
   );
